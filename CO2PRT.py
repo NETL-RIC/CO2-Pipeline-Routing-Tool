1258ce24004a5e7c123f13b0b056f22c44c6e0c9
Dakota Zaengle <zaengled@gmail.com>
2024-06-06 08:51:30 -0700

    adding minimum pyinstaller files

---
+++ CO2PRT.py
@@ -0,0 +1,10 @@
+"""
+'main' file outside of module to help pyinstaller build an exe and run the flask service
+
+    Builds from inside the module cause issues with relative imports.
+    Run also needed to be called on flask instead of using the flask cmd line call.
+"""
+
+from Flask import base
+
+base.api.run()
